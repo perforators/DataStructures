385c91d2d630fda3db7b562f456e7868e4ba9d55
Egor Krivochkov <krivochkov01@mail.ru>
2024-06-06 21:03:32 +0300

    - Added method `Pool.poll(timeout)`.

--- src/main/kotlin/io/github/perforators/pools/Pool.kt
+++ src/main/kotlin/io/github/perforators/pools/Pool.kt
@@ -11,6 +11,16 @@ interface Pool<out T> {
     suspend fun poll(): T?
 
     /**
+     * Retrieves and removes the element of this pool, waiting up to the
+     * specified wait time if necessary for an element to become available.
+     *
+     * @param timeoutMillis how long to wait before giving up, in millis
+     * @return the element of this pool, or null if the
+     *         specified waiting time elapses before an element is available
+     */
+    suspend fun poll(timeoutMillis: Long): T?
+
+    /**
      * Retrieves and removes the element of this pool, waiting if necessary
      * until an element becomes available.
      *
